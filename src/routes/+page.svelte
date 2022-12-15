92b90f76039af586a7ef0fc9349e699d081fb91a
kamesuke42 <kamesuke42@kamesuke42.com>
2022-12-16 00:05:44 +0900

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Happy holidays!</h1>
+
+<a href="/about">About author</a>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,14 @@
+<h2>About</h2>
+
+<div>
+  <div>
+    <a href="https://kamesuke42.com">Author: kamesuke42</a>
+  </div>
+  <div>
+    GitHub: <a href="https://github.com/kamesuke42">kamesuke42</a>
+  </div>
+
+  <div>
+    <a href="/">Back to index page.</a>
+  </div>
+</div>
